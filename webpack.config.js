4f1d34e39989be619d1fde120b9d9314950eb005
Calvin Yau <calvinj.yau@gmail.com>
2017-01-24 03:20:28 -0800

    Add package.json and webpack config

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+
+module.exports = {
+  context: __dirname,
+  entry: './lib/frogger.jsx',
+  output: {
+    path: "./lib",
+    filename: 'bundle.js',
+  },
+  resolve: {
+    extensions: ['', '.js'],
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel',
+        query: {
+          presets: ['es2015'],
+        }
+      }
+    ]
+  },
+  devtool: 'source-maps',
+  externals: {
+    "createjs": "createjs",
+  },
+};
